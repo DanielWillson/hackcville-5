94d59842c948ea57974796840d5f575b77c364a4
Daniel WIllson <daniel@danielwillson.me>
2017-08-07 13:47:34 -0400

    small tweaks

---
+++ single-program.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package HackCville_5.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			echo get_post_meta(get_the_ID(), 'program_topic', true);
+
+			get_template_part( 'template-parts/content', get_post_format() );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
