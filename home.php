fec476b974b429daa7e8606a50cfa1536c822c8e
Daniel WIllson <daniel@danielwillson.me>
2017-08-25 12:09:09 -0400

    added back temporary cat-hero to home

--- home.php
+++ home.php
@@ -20,10 +20,19 @@ include ($template_url);
 <div class="pioneer-home page pioneer" id="pioneer-check">
 	<div class="white-bg">
 		<div class="container">
-				
-			<?php
+			<div class="intro">
+				<h2>Recent Stories</h2>
+			</div>
+			<div class="flex">
+				<?php
 
-			while ( have_posts() ) : the_post();
+			
+				$args = array( 
+					'post_type' => 'post',
+					'posts_per_page' => 4 );
+				$loop = new WP_Query( $args );
+				/* Requests the posts via The Loop */
+				while ( $loop->have_posts() ) : $loop->the_post(); 
 
 					
 				if (has_post_thumbnail( $post->ID ) ) {
@@ -31,7 +40,7 @@ include ($template_url);
 					$f_i = $f_i[0]; 
 				} ?>
 
-				<div class="story flex">
+				<div class="story flex-1-of-2 flex">
 					<div class="flex-1-of-2">
 						<a class="image-link" href="<?php the_permalink(); ?>">
 							<img src="<?php echo $f_i; ?>">
@@ -44,7 +53,7 @@ include ($template_url);
 						<div class="excerpt">
 							<?php echo apply_filters('the_excerpt', get_post_field('post_excerpt', $post_id)); ?>
 						</div>
-						<a href="<?php the_permalink(); ?>" class="button">
+						<a href="<?php the_permalink(); ?>" class="">
 							Read more &rarr;
 						</a>
 					</div>
@@ -59,6 +68,49 @@ include ($template_url);
 			the_posts_navigation();
 
 			?>
+
+			</div>
+<?php 
+// Sets up $current_category and $current_category_headline for the category hero (cat-hero)
+// $current_category is the category passed to the hero
+// $current_category_headline is the text passed to the headline seen on the site
+$current_category = '';
+$current_category_headline = 'Entrepreneurship Stories';
+$current_category = 'Entrepreneurship';
+
+$template_url = get_template_directory();
+$template_url .= '/template-parts/cat-hero.php';
+
+// Works like the Starkers Utilities function and includes the cat-hero in this doc.
+// Must use this include() function instead of Starkers so that the variable passing can work
+include ($template_url);
+
+// Resets the 2 category variables and imports again to create the technology section
+$current_category_headline = 'Technology Stories';
+$current_category = 'Technology';
+include ($template_url);
+
+// Imports the newsletter subscribe section
+// ADD IN NEWSLETTER
+
+// Resets the 2 category variables and imports again to create the art section
+$current_category_headline = 'Art and Design Stories';
+$current_category = 'Art';
+include ($template_url);
+
+// Resets the 2 category variables and imports again to create the music section
+$current_category_headline = 'Music Stories';
+$current_category = 'Music';
+include ($template_url);
+
+// Resets the 2 category variables and imports again to create the HackCville section
+$current_category_headline = 'HackCville Stories';
+$current_category = 'HackCville';
+include ($template_url);
+
+
+	?>			
+			
 				
 			
 		</div>
