fccb8259292feedf38adcc4834531b4fad71ef43
Daniel WIllson <daniel@danielwillson.me>
2017-11-12 18:18:47 -0500

    created v1 of the Census page

---
+++ page-census.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Template Name: Census Information
+ *The template for displaying Launch Track archives
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package HackCville_5.0
+ */
+
+// Import Launch Academy Nav
+get_header(); 
+?>
+<div class="page">
+	<div class="hc-blue-bg">
+		<div class="container">
+			<div class="flex">
+				<div class="flex-1-of-1">
+					<h1>How to stay involved at HC this Spring</h1>
+					<h3>Learn about your 5 options for continuing to learn, build, start, and lead</h3>
+				</div>
+				<div class="flex-3-of-5">
+					<p>After your first semester at HackCville, there are a lot of different ways you can continue to stay involved:
+						<ol>
+							<li>Join (or continue) with HC Leadership</li>
+							<li>Team Challenges (new!)</li>
+							<li>Take another program</li>
+							<li>Work on a personal project/startup</li>
+							<li>Work with a partner organization</li>
+							<li>Go abroad or go inactive</li>
+						</ol>
+					These are all outlined in detail below! Once you're ready to pick and your questions are answered, you submit the <span style="font-weight: bold;">HackCville Census</span>, which enrolls you in the option you've selected. (This is kind of like our hopefully-less-shitty version of SIS.)</p>
+					<p>The Census needs to be completed by 11:59pm on Monday, November 27 (right after Thanksgiving). It is mandatory for all members, even if you're picking option 6 above.</p>
+				</div>
+				<div class="flex-2-of-5">
+					<br>
+					<img src="<?php echo get_home_url(); ?>/wp-content/themes/hackcville-5-0/images/census-screenshot.png">
+					<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+						View the Census &rarr;
+					</a>
+					<p>Have questions about the process? Email daniel@hackcville.com or Slack @daniel.</p>
+				</div>
+			</div>
+			<?php
+				// while ( have_posts() ) : the_post();
+				// get_template_part( 'template-parts/content', 'page' );
+				// endwhile; // End of the loop.
+			?>
+		</div>
+	</div>
+</div>
+<div class="leadership-option">
+	<div class="white-bg">
+		<div class="container section-border">
+			<h1>1) Join (or continue) with HC Leadership.</h1>
+			<p>Applications for this were due on Sunday, November 11. If you’re super-interested and you forgot, we have quietly extended the deadline through Monday, November 12. Details are <a href="http://hackcville.com/leadership" target="_blank">here</a>.</p>
+			<p>Even if you've been accepted into leadership or you're on staff, you still need to fill out the Census. There will be an option to signify that you're already part of Leadership.</p>
+			<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+				Fill out the Census &rarr;
+			</a>
+			<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+				Questions? Email daniel@hackcville.com &rarr;
+			</a>
+		</div>
+	</div>
+</div>
+<div class="program-challenge-option">
+	<div class="white-bg">
+		<div class="container section-border">
+		<h1>Or, keep learning through challenges or programs.</h1>
+			<div class="flex">
+				<div class="flex-1-of-2">
+					<h2>2) TEAM CHALLENGES</h2>
+					<p style="font-style: italic;">A new, flexible, member-only way to learn new skills while building toward a fun end-of-semester challenge</p>
+				</div>
+				<div class="flex-1-of-2">
+					<h2>3) PROGRAMS</h2>
+					<p style="font-style: italic;">Our traditional program structure to introduce anyone to the basics of a technical or entrepreneurial skill</p>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>Who challenges are for:</h4>
+					<ul>
+						<li>You’re a self-motivated learner, or want to become one</li>
+						<li>You're ready for a new experience after taking a HackCville program</li>
+						<li>You have a strong curiosity around a particular topic, even if you’re a complete newbie</li>
+					</ul>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>Who programs are for:</h4>
+					<ul>
+						<li>You learn best with lots of structure and support</li>
+						<li>You’re completely new to a topic</li>
+						<li>You want to explore a topic to see if it’s something you really enjoy</li>
+					</ul>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>Challenge topic options:</h4>
+					<p>You'll pick one or more of these on the Census: videography, photography, web design, software dev, graphic design, data science.</p>
+					<p>You can use Challenges to get started in a topic or use them to advance your existing skills. You can also suggest a new topic, like beer or cryptocurrency.</p>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>Program topic options:</h4>
+					<p>Meeting times and options for spring programs are available at <a href="http://hackcville.com/programs" target="_blank">hackcville.com/programs</a>. We have two new programs next semester: marketing and sales. (Fun names TBD.)</h4>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>How challenges work:</h4>
+					<ul>
+						<li>You tell us which topic(s) you’re interested in on the Census</li>
+						<li>We place you into a team of 4-5 other members. You can request to be put in a team with friends. You work with that team each week to teach each other what you want to learn.</li>
+						<li>We give you a HackCville coach who will give you some initial structure, guidance, and resources to learn.</li> 
+						<li>We work with you to decide on a big challenge for the end of the semester to demonstrate what you've learned. These are different depending on the topic. (Make a cryptocurrency trading bot, brew your own beer, redesign an app, etc.)</li>
+					</ul>
+					<p>We expect about 100-120 existing members to participate in challenges.</p>
+				</div>
+				<div class="flex-1-of-2">
+					<h4>How programs work:</h4>
+					<ul>
+						<li>You tell us which program you want to take. You must be available at the regular meeting times, listed below.</li>
+						<li>You’ll have a brief interview in January to ensure that you’re a great fit for the program. Depending on how this goes, we may recommend another method of involvement next semester, or that you join a team challenge instead.</li>
+						<li>You meet twice each week at the regular times for the semester.</li>
+					</ul>
+					<p>We expect about 30-50 existing members to participate in programs.</p>
+				</div>
+				
+				<div class="flex-1-of-1">
+					<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+						Fill out the Census &rarr;
+					</a>
+					<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+						Questions? Email kate@hackcville.com &rarr;
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
+<div class="program-challenge-option">
+	<div class="white-bg">
+		<div class="container section-border">
+			<h1>4) Work on a personal project or startup.</h1>
+			<p>Regardless of what stage you're at, we can help you take the next step forward with your project, startup, non-profit, or student organization.</p>
+			<p>In the Census we'll ask for a little bit of information about where you're at. With that information we'll place you either into Alpha or Beta. These are our two programs for project support that are run by Andy Page and Chip Ransler, respectively.</p>
+			<p>(If you're an Elliewood Fellow, select this option.)</p>
+			<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+				Fill out the Census &rarr;
+			</a>
+			<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+				Questions? Email andy@hackcville.com &rarr;
+			</a>
+			
+		</div>
+	</div>
+</div>
+<div class="program-challenge-option">
+	<div class="white-bg">
+		<div class="container section-border">
+			<h1>5) Work with a partner organization.</h1>
+			<p>If you <span style="font-style: italic;">already</span> work with Radical Advertising, ReinventED Lab, Tom Tom Founders Festival, or Greens to Grounds, this is another way you can maintain active membership.</p>
+			<p>The Census is not how you apply to these organizations - they all have separate recruiting timelines and processes.</p>
+			<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+				Fill out the Census &rarr;
+			</a>
+			<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+				Questions? Email yash@hackcville.com &rarr;
+			</a>
+			
+		</div>
+	</div>
+</div>
+<div class="program-challenge-option">
+	<div class="white-bg">
+		<div class="container section-border">
+			<h1>6) Go abroad or go inactive.</h1>
+			<p>We still need you to tell us via the Census if you're planning to go abroad or become an inactive member next semester. All inactive members are welcome to come back in any future semester.</p>
+			<a href="https://hackcville.typeform.com/to/KLmOFH" target="_blank" class="button">
+				Fill out the Census &rarr;
+			</a>
+			<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+				Questions? Email daniel@hackcville.com &rarr;
+			</a>
+		</div>
+	</div>
+</div>
+<div class="dues-info">
+	<div class="blue-bg">
+		<div class="container">
+			<h1>Membership dues are $50.</h1>
+			<p>At the end of the Census, you'll be asked to pay next semester's $50 membership dues by credit card. These help us cover the cost of running HackCville.</p>
+			<p>These dues can be waived (via the Census) to anyone with financial need, no questions asked. We ask that you do not abuse this policy - dues are a vital form of income for us.</p> 
+			<a href="mailto:daniel@hackcville.com" target="_blank" class="button">
+				Questions? Email daniel@hackcville.com &rarr;
+			</a>
+		</div>
+	</div>
+</div>
+
+
+<?php
+get_footer();
